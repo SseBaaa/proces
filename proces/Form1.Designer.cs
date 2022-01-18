e633b2ca3e296ec6f08b1ecc258a2798b6267778
SseBaaa <89750702+SseBaaa@users.noreply.github.com>
2022-01-18 13:09:26 +0100

    Add files via upload

---
+++ proces/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace proces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonInternet = new System.Windows.Forms.Button();
+            this.buttonExcel = new System.Windows.Forms.Button();
+            this.buttonWord = new System.Windows.Forms.Button();
+            this.buttonInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonInternet
+            // 
+            this.buttonInternet.Font = new System.Drawing.Font("MV Boli", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInternet.Location = new System.Drawing.Point(12, 12);
+            this.buttonInternet.Name = "buttonInternet";
+            this.buttonInternet.Size = new System.Drawing.Size(372, 191);
+            this.buttonInternet.TabIndex = 0;
+            this.buttonInternet.Text = "internet";
+            this.buttonInternet.UseVisualStyleBackColor = true;
+            // 
+            // buttonExcel
+            // 
+            this.buttonExcel.Font = new System.Drawing.Font("MV Boli", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExcel.Location = new System.Drawing.Point(390, 12);
+            this.buttonExcel.Name = "buttonExcel";
+            this.buttonExcel.Size = new System.Drawing.Size(398, 191);
+            this.buttonExcel.TabIndex = 1;
+            this.buttonExcel.Text = "Excel";
+            this.buttonExcel.UseVisualStyleBackColor = true;
+            // 
+            // buttonWord
+            // 
+            this.buttonWord.Font = new System.Drawing.Font("MV Boli", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonWord.Location = new System.Drawing.Point(12, 209);
+            this.buttonWord.Name = "buttonWord";
+            this.buttonWord.Size = new System.Drawing.Size(372, 229);
+            this.buttonWord.TabIndex = 2;
+            this.buttonWord.Text = "word";
+            this.buttonWord.UseVisualStyleBackColor = true;
+            this.buttonWord.Click += new System.EventHandler(this.buttonWord_Click);
+            // 
+            // buttonInfo
+            // 
+            this.buttonInfo.Font = new System.Drawing.Font("MV Boli", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInfo.Location = new System.Drawing.Point(390, 209);
+            this.buttonInfo.Name = "buttonInfo";
+            this.buttonInfo.Size = new System.Drawing.Size(398, 229);
+            this.buttonInfo.TabIndex = 3;
+            this.buttonInfo.Text = "info";
+            this.buttonInfo.UseVisualStyleBackColor = true;
+            this.buttonInfo.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonInfo);
+            this.Controls.Add(this.buttonWord);
+            this.Controls.Add(this.buttonExcel);
+            this.Controls.Add(this.buttonInternet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonInternet;
+        private System.Windows.Forms.Button buttonExcel;
+        private System.Windows.Forms.Button buttonWord;
+        private System.Windows.Forms.Button buttonInfo;
+    }
+}
+
---
+++ proces/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonWord_Click(object sender, EventArgs e)
+        {
+            Process.Start("winword.exe");
+        }
+    }
+}
